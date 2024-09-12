c5edcdcfde4556c953ee8eada745afaec8d8d5eb
Md Zahiruddin <3786zahirkhan@gmail.com>
2024-09-12 09:20:32 +0530

    website completed

---
+++ src/components/IntroVideo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import video from "../assets/MBA Chai Wala.mp4";
+
+const IntroVide = () => {
+  return (
+
+    <div className="intro">
+      
+      <video src={video} muted autoplay loop controlsList="nodownload"></video>
+      <div></div>
+    </div>
+  );
+  
+};
+
+export default IntroVide;
